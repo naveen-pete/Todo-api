ebb99233bc78ff06f92a847caf92497aedff21ca
Naveen Pete <naveen.shenoy.blr@gmail.com>
2016-03-10 12:24:22 +0530

    Add todo model and POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -56,18 +57,24 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res) {
 	var newTodo = _.pick(req.body, 'description', 'completed');
 
-	if (!_.isBoolean(newTodo.completed) ||
-		!_.isString(newTodo.description) ||
-		newTodo.description.trim().length <= 0) {
-		return res.status(400).send();
-	}
+	db.todo.create(newTodo).then(function(todo) {
+		res.json(todo.toJSON());
+	}).catch(function (e) {
+		res.status(400).json(e);
+	});
 
-	newTodo.description = newTodo.description.trim();
-	newTodo.id = todoNextId++;
+	// if (!_.isBoolean(newTodo.completed) ||
+	// 	!_.isString(newTodo.description) ||
+	// 	newTodo.description.trim().length <= 0) {
+	// 	return res.status(400).send();
+	// }
 
-	todos.push(newTodo);
+	// newTodo.description = newTodo.description.trim();
+	// newTodo.id = todoNextId++;
 
-	res.json(newTodo);
+	// todos.push(newTodo);
+
+	// res.json(newTodo);
 });
 
 app.delete('/todos/:id', function(req, res) {
@@ -116,6 +123,8 @@ app.put('/todos/:id', function(req, res) {
 	res.json(matchedTodo);
 });
 
-app.listen(PORT, function() {
-	console.log('Express server started and listening on port ' + PORT + '!');
+db.sequelize.sync().then(function() {
+	app.listen(PORT, function() {
+		console.log('Express server started and listening on port ' + PORT + '!');
+	});
 });
